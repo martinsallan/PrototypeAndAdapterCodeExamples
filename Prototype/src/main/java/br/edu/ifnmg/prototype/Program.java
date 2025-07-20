374fc3128e6973bae47c0afc19ebda90d5986638
Allan Martins <martinsallan111@gmail.com>
2025-07-20 12:21:59 -0300

    Renomeação para Program e correção de exibição de caracteres inválidos

--- Prototype/src/main/java/br/edu/ifnmg/prototype/Program.java
+++ Prototype/src/main/java/br/edu/ifnmg/prototype/Program.java
@@ -5,7 +5,7 @@ package br.edu.ifnmg.prototype;
  * @author marti
  */
 
-public class Demo {
+public class Program {
     public static void main(String[] args) {
         //Carrega os protótipos iniciais no registro
         PrototypeRegistry.loadCache();
@@ -13,17 +13,17 @@ public class Demo {
         //Obtém um clone do círculo
         Shape clonedCircle = PrototypeRegistry.getShape("1");
         clonedCircle.setColor("Azul");
-        System.out.println("Clone do Círculo: " + clonedCircle.getType() + ", Cor: " + clonedCircle.getColor());
+        System.out.println("Clone do Circulo: " + clonedCircle.getType() + ", Cor: " + clonedCircle.getColor());
 
         //Obtém outro clone, desta vez do retângulo
         Shape clonedRectangle = PrototypeRegistry.getShape("2");
         clonedRectangle.setColor("Vermelho");
-        System.out.println("Clone do Retângulo: " + clonedRectangle.getType() + ", Cor: " + clonedRectangle.getColor());
+        System.out.println("Clone do Retangulo: " + clonedRectangle.getType() + ", Cor: " + clonedRectangle.getColor());
 
         //Verificando que são objetos diferentes
         Shape clonedCircle2 = PrototypeRegistry.getShape("1");
         clonedCircle2.setColor("Verde");
-        System.out.println("Clone 2 do Círculo: " + clonedCircle2.getType() + ", Cor: " + clonedCircle2.getColor());
-        System.out.println("Cor do primeiro clone do círculo ainda é: " + clonedCircle.getColor());
+        System.out.println("Clone 2 do Circulo: " + clonedCircle2.getType() + ", Cor: " + clonedCircle2.getColor());
+        System.out.println("Cor do primeiro clone do circulo ainda e: " + clonedCircle.getColor());
     }
 }
